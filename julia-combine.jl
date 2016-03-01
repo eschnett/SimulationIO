72ba5e6d841a5c035e753b5b86140341708e5e87
Erik Schnetter <schnetter@gmail.com>
2016-03-01 11:47:07 -0500

    Add Julia script to combine HDF5 datasets

---
+++ julia-combine.jl
@@ -0,0 +1,349 @@
+#! /usr/bin/env julia
+
+using PyCall
+
+@pyimport H5
+@pyimport RegionCalculus as RC
+@pyimport SimulationIO as SIO
+
+using HDF5
+
+
+
+const indent_level = Ref{Int}(0)
+indent() = indent_level[] += 2
+outdent() = indent_level[] -= 2
+message(msgs...) = println(" "^indent_level[], msgs...)
+
+
+
+# Combine multiple discretization boxes into one
+filename = "cactus/iof5-uniform-p2.s5"
+filename2 = "cactus/iof5-uniform-p2-combined.s5"
+
+# Read project
+file = H5.H5File()
+file[:openFile](filename, H5.H5F_ACC_RDONLY, H5.FileAccPropList())
+message("Reading project...")
+project = SIO.readProject(file)
+indent()
+message("Project \"$(project[:name])\"")
+outdent()
+file[:close]()
+
+# Create project
+message()
+message("Copying metadata...")
+project2 = SIO.createProject(project[:name])
+
+# Copy parameters
+indent()
+for parameter in project[:parameters][:values]()
+    message("Parameter \"$(parameter[:name])\"")
+    parameter2 = project2[:createParameter](parameter[:name])
+
+    indent()
+    for parametervalue in parameter[:parametervalues][:values]()
+        message("ParameterValue \"$(parametervalue[:name])\"")
+        parametervalue2 =
+            parameter2[:createParameterValue](parametervalue[:name])
+        # TODO: Set actual parameter value
+    end
+    outdent()
+end
+outdent()
+
+# Copy configurations
+indent()
+for configuration in project[:configurations][:values]()
+    message("Configuration \"$(configuration[:name])\"")
+    configuration2 = project2[:createConfiguration](configuration[:name])
+
+    indent()
+    for parametervalue in configuration[:parametervalues][:values]()
+        message("ParameterValue \"$(parametervalue[:name])\"")
+        parameter = parametervalue[:parameter][:lock]()
+        parameter2 = get(project2[:parameters], parameter[:name])
+        parametervalue2 =
+            get(parameter2[:parametervalues], parametervalue[:name])
+        configuration2[:insertParameterValue](parametervalue2)
+    end
+    outdent()
+end
+
+# Copy tensor types
+for tensortype in project[:tensortypes][:values]()
+    message("TensorType \"$(tensortype[:name])\"")
+    tensortype2 =
+        project2[:createTensorType](tensortype[:name], tensortype[:dimension],
+            tensortype[:rank])
+
+    indent()
+    for tensorcomponent in tensortype[:tensorcomponents][:values]()
+        message("TensorComponent \"$(tensorcomponent[:name])\"")
+        tensorcomponent2 =
+            tensortype2[:createTensorComponent](tensorcomponent[:name],
+                tensorcomponent[:storage_index], tensorcomponent[:indexvalues])
+    end
+    outdent()
+end
+outdent()
+
+# Copy manifolds
+indent()
+for manifold in project[:manifolds][:values]()
+    message("Manifold \"$(manifold[:name])\"")
+    configuration2 =
+        get(project2[:configurations], manifold[:configuration][:name])
+    manifold2 =
+        project2[:createManifold](manifold[:name], configuration2,
+            manifold[:dimension])
+
+    indent()
+    for discretization in manifold[:discretizations][:values]()
+        message("Discretization \"$(discretization[:name])\"")
+        configuration2 =
+            get(project2[:configurations],
+                discretization[:configuration][:name])
+        discretization2 =
+            manifold2[:createDiscretization](discretization[:name],
+                configuration2)
+        # TODO: Handle subdiscretizations
+
+        # Read and combine discretizationblocks
+        regions = []
+        actives = []
+        indent()
+        for discretizationblock in
+                discretization[:discretizationblocks][:values]()
+            message("DiscretizationBlock \"$(discretizationblock[:name])\"")
+            push!(regions, discretizationblock[:region])
+            push!(actives, discretizationblock[:active])
+        end
+        outdent()
+        combined_region = RC.ibox(manifold[:dimension])
+        for region in regions
+            combined_region = combined_region[:bounding_box](region)
+        end
+        message("combined_region: $(combined_region[:__str__]())")
+        combined_active = RC.iregion(manifold[:dimension])
+        for active in actives
+            combined_active = combined_active[:union](active)
+        end
+        message("combined_active: $(combined_region[:__str__]())")
+
+        discretizationblock2 =
+            discretization2[:createDiscretizationBlock]("discretizationblock")
+        discretizationblock2[:region] = combined_region
+        discretizationblock2[:active] = combined_active
+    end
+    outdent()
+end
+outdent()
+
+# Copy tangentspaces
+indent()
+for tangentspace in project[:tangentspaces][:values]()
+    message("TangentSpace \"$(tangentspace[:name])\"")
+    configuration2 =
+        get(project2[:configurations], tangentspace[:configuration][:name])
+    tangentspace2 =
+        project2[:createTangentSpace](tangentspace[:name], configuration2,
+            tangentspace[:dimension])
+
+    indent()
+    for basis in tangentspace[:bases][:values]()
+        message("Basis \"$(basis[:name])\"")
+        configuration2 =
+            get(project2[:configurations], basis[:configuration][:name])
+        basis2 = tangentspace2[:createBasis](basis[:name], configuration2)
+    end
+    outdent()
+end
+outdent()
+
+# Copy fields
+indent()
+for field in project[:fields][:values]()
+    message("Field \"$(field[:name])\"")
+    configuration2 =
+        get(project2[:configurations], field[:configuration][:name])
+    manifold2 = get(project2[:manifolds], field[:manifold][:name])
+    tangentspace2 = get(project2[:tangentspaces], field[:tangentspace][:name])
+    tensortype2 = get(project2[:tensortypes], field[:tensortype][:name])
+    field2 =
+        project2[:createField](field[:name], configuration2, manifold2,
+            tangentspace2, tensortype2)
+
+    indent()
+    for discretefield in field[:discretefields][:values]()
+        message("DiscreteField \"$(discretefield[:name])\"")
+        configuration2 =
+            get(project2[:configurations], discretefield[:configuration][:name])
+        discretization2 =
+            get(manifold2[:discretizations],
+                discretefield[:discretization][:name])
+        basis2 = get(tangentspace2[:bases], discretefield[:basis][:name])
+        discretefield2 =
+            field2[:createDiscreteField](discretefield[:name],
+                configuration2, discretization2, basis2)
+
+        if !isempty(discretefield[:discretefieldblocks])
+            discretizationblock2 =
+                get(discretization2[:discretizationblocks],
+                    "discretizationblock")
+            discretefieldblock2 =
+                discretefield2[:createDiscreteFieldBlock](
+                    "discretefieldblock", discretizationblock2)
+
+            for tensorcomponent2 in tensortype2[:tensorcomponents][:values]()
+                discretefieldblockcomponent2 =
+                    discretefieldblock2[:createDiscreteFieldBlockComponent](
+                        tensorcomponent2[:name], tensorcomponent2)
+            end
+        end
+
+    end
+    outdent()
+end
+outdent()
+
+# Copy coordinatesystems
+indent()
+for coordinatesystem in project[:coordinatesystems][:values]()
+    message("CoordinateSystem \"$(coordinatesystem[:name])\"")
+    configuration2 =
+        get(project2[:configurations], coordinatesystem[:configuration][:name])
+    manifold2 = get(project2[:manifolds], coordinatesystem[:manifold][:name])
+    coordinatesystem2 =
+        project2[:createCoordinateSystem](coordinatesystem[:name],
+            configuration2, manifold2)
+
+    indent()
+    for coordinatefield in coordinatesystem[:coordinatefields][:values]()
+        message("CoordinateField \"$(coordinatefield[:name])\"")
+        field2 = get(project2[:fields], coordinatefield[:field][:name])
+        coordinatefield2 =
+            coordinatesystem2[:createCoordinateField](coordinatefield[:name],
+                coordinatefield[:direction], field2)
+    end
+    outdent()
+end
+outdent()
+
+# Write project
+cpl = H5.FileCreatPropList()
+apl = H5.FileAccPropList()
+apl[:setFcloseDegree](H5.H5F_CLOSE_STRONG)
+apl[:setLibverBounds](H5.H5F_LIBVER_LATEST, H5.H5F_LIBVER_LATEST)
+file2 = H5.H5File(filename2, H5.H5F_ACC_TRUNC, cpl, apl)
+project2[:write](file2)
+file2[:close]()
+
+message()
+message("Copying data..")
+
+hfile = h5open(filename, "r")
+cpl = p_create(HDF5.H5P_FILE_CREATE)
+apl = p_create(HDF5.H5P_FILE_ACCESS)
+apl["fclose_degree"] = HDF5.H5F_CLOSE_STRONG
+apl["libver_bounds"] = HDF5.H5F_LIBVER_LATEST, HDF5.H5F_LIBVER_LATEST
+hfile2 = h5open(filename2, "r+", cpl=cpl, apl=apl)
+
+
+
+# Copy and combine datasets
+indent()
+for field2 in project2[:fields][:values]()
+    message("Field \"$(field2[:name])\"")
+    field = get(project[:fields], field2[:name])
+
+    indent()
+    for discretefield2 in field2[:discretefields][:values]()
+        message("DiscreteField \"$(discretefield2[:name])\"")
+        discretefield = get(field[:discretefields], discretefield2[:name])
+
+        # Loop over datasets to be written
+        indent()
+        for discretefieldblock2 in
+                discretefield2[:discretefieldblocks][:values]()
+            discretizationblock2 = discretefieldblock2[:discretizationblock]
+
+            for discretefieldblockcomponent2 in
+                    discretefieldblock2[:discretefieldblockcomponents][
+                        :values]()
+                tensorcomponent2 =
+                    discretefieldblockcomponent2[:tensorcomponent]
+                message("TensorComponent \"$(tensorcomponent2[:name])\"")
+
+                # Create dataset
+                path2 = discretefieldblockcomponent2[:getPath]()
+                name2 = discretefieldblockcomponent2[:getName]()
+                # message("path2=$path2 name2=$name2")
+                lower2 = discretizationblock2[:region][:lower]()
+                upper2 = discretizationblock2[:region][:upper]()
+                shape2 = discretizationblock2[:region][:shape]()
+                message("combined_region=$lower2:$upper2")
+                # 16^3 * 8 B = 32 kB
+                # chunksize = fill(min(16,i), dsize)
+                chunksize = HDF5.heuristic_chunk(Float64, shape2)
+                # shuffling improves compression
+                # level 1 is fast, but still offers good compression
+                clevel = 1
+                dataset =
+                    d_create(hfile2, "$path2/$name2",
+                        datatype(Float64), dataspace(shape2...),
+                        "chunk", chunksize, "shuffle", (), "compress", clevel)
+
+                # Loop over datasets to be read
+                indent()
+                for discretefieldblock in
+                        discretefield[:discretefieldblocks][:values]()
+                    message("DiscreteFieldBlock " *
+                        "\"$(discretefieldblock[:name])\"")
+                    discretizationblock =
+                        discretefieldblock[:discretizationblock]
+
+                    discretefieldblockcomponent =
+                        get(discretefieldblock[:storage_indices],
+                            tensorcomponent2[:storage_index])
+                    message("DiscreteFieldBlockComponent " *
+                        "\"$(discretefieldblockcomponent[:name])\"")
+
+                    tensorcomponent =
+                        discretefieldblockcomponent[:tensorcomponent]
+                    @assert tensorcomponent[:storage_index] ==
+                        tensorcomponent2[:storage_index]
+
+                    # Read dataset
+                    path = discretefieldblockcomponent[:getPath]()
+                    name = discretefieldblockcomponent[:getName]()
+                    # message("path=$path name=$name")
+                    lower = discretizationblock[:region][:lower]()
+                    upper = discretizationblock[:region][:upper]()
+                    shape = discretizationblock[:region][:shape]()
+                    message("region=$lower:$upper")
+                    data = read(hfile, "$path/$name")
+                    @assert size(data) == shape
+
+                    # Write into dataset
+                    dataset[lower[1]+1:upper[1], lower[2]+1:upper[2],
+                        lower[3]+1:upper[3]] =
+                        data
+                end
+                outdent()
+
+            end
+        end
+        outdent()
+
+    end
+    outdent()
+end
+outdent()
+
+close(hfile2)
+close(hfile)
+
+message()
+message("Done.")
