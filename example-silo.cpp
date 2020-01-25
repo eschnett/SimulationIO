0ca16c07fb3ee59077bc0105063c5ceb339fdab2
Erik Schnetter <schnetter@gmail.com>
2020-01-25 17:50:52 -0500

    Code development

--- example-silo.cpp
+++ example-silo.cpp
@@ -1,12 +1,12 @@
 #include <silo.h>
 
+#include <cassert>
 #include <iostream>
-
 using namespace std;
 
 int main(int argc, char **argv) {
   cout << "example-silo: Create a Silo file\n";
-  DBFile *file = DBCreate("example.silo", DB_NOCLOBBER, DB_LOCAL,
+  DBfile *file = DBCreate("example.silo", DB_NOCLOBBER, DB_LOCAL,
                           "Example Silo file for SimulationIO", DB_HDF5);
   const int ierr = DBClose(file);
   assert(!ierr);
