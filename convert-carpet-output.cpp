3535cc49e5d047716797feb42531d9ef29c9bfc1
Erik Schnetter <schnetter@gmail.com>
2023-03-27 09:24:03 -0400

    Explicitly convert from vector to point_t

--- convert-carpet-output.cpp
+++ convert-carpet-output.cpp
@@ -632,7 +632,7 @@ int main(int argc, char **argv) {
             assert(rank == dim);
             dataspace.getSimpleExtentDims((hsize_t *)(shape.data()));
             std::reverse(shape.begin(), shape.end());
-            discretizationblock->setBox(box_t(offset, point_t(offset) + shape));
+            discretizationblock->setBox(box_t(offset, point_t(offset) + point_t(shape)));
 
             if (active.valid()) {
               discretizationblock->setActive(active);
